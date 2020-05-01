f0f87e9f7edf91e33c0a82278d9c63d8986bccbd
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-04-30 22:08:39 -0300

    Separando backend e frontend
    Trabalhando melhor a separação de dados d=entre backend e frontend por uma engine. Exercendo os conceitos

---
+++ data.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: "starter",
+        class: "card change_borderst",
+        title: '<h1 id="title_starter">Starter</h1>',
+        image: 'src="https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg"  alt="Starter Logo"',
+        info: '<strong id="strong_star">Become a desired programmer</strong> on the market with these free courses',
+        modules: "5 modules",
+        price: "Free"
+    },
+
+    {
+        id: "launchbase",
+        class: "card change_borderla",
+        title: '<h1 id="title_launch">Launchbase</h1>',
+        image: 'id="image_launch" src="https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg"  alt="LaunchBase Logo"',
+        info: '<strong id="strong_launch">Master programming from scratch</strong> and have access to the best opportunities in the market',
+        modules: "7 modules",
+        price: "Paid out"
+    },
+
+    {
+        id: "gostack",
+        class: "card change_bordergo",
+        title: '<h1 id="title_stack">Gostack</h1>',
+        image: 'id="image_go" src="https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg"  alt="GoStack Logo"',
+        info: '<strong id="strong_stack">Immersive training</strong> in the most modern web and mobile development technologies',
+        modules: "15 modules",
+        price: "Paid out"
+    }
+
+]
--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const infos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function (req, res) {
-    return res.render("rocket_about")
+    const data = {
+        title: "Rocketseat",
+        subTitle: "Technology company",
+        info: "Brasilian company focused on the best market technologies and willing to carry its pupils to the <strong>next level</strong>",
+        tecImage: 'src="assets/rocket_trofy.png" alt="rockettrofy"',
+        tecTitle: "The same tecnologies used by renowned companies in the market",
+        tecSubTitle: "Tecnologies:",
+        tecs: "<li>React</li><li>React Native</li><li>Node</li><li>JavaScript</li><li>entre outras</li>"
+    }
+
+    return res.render("rocket_about", {data})
 })
 
 server.get("/rocket_content", function (req, res) {
-    return res.render("rocket_content")
+    return res.render("rocket_content", {infos})
 })
 
 server.use(function(req, res) {
